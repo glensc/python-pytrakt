78a1f7eb93ef04e1f151641bf7486c4b8b4c0889
simonc56 <simchat@gmail.com>
2026-05-25 14:52:20 +0200

    reset pagination store before raise_if_needed() call

--- trakt/api.py
+++ trakt/api.py
@@ -133,8 +133,8 @@ class HttpClient:
         else:
             response = self.session.request(method, url, headers=self.headers, auth=self.auth, timeout=self.timeout, data=json.dumps(data))
         self.logger.debug('RESPONSE [%s] (%s): %s', method, url, str(response))
+        _pagination_store.set(None)
         if response.status_code == 204:  # HTTP no content
-            _pagination_store.set(None)
             return None
         self.raise_if_needed(response)
         _pagination_store.set(self._parse_pagination_headers(response.headers))
